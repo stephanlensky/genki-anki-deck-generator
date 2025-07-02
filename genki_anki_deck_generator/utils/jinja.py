d7013fdf9cbaf5305f04d9c478bae2adc58244ea
Stephan Lensky <stephanl.public@gmail.com>
2025-07-02 14:47:51 -0400

    Fix escaping

--- genki_anki_deck_generator/utils/jinja.py
+++ genki_anki_deck_generator/utils/jinja.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from typing import Any
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
 TEMPLATES_DIR = Path("templates")
-ENV = Environment(loader=FileSystemLoader(TEMPLATES_DIR), autoescape=select_autoescape())
+ENV = Environment(loader=FileSystemLoader(TEMPLATES_DIR), autoescape=False)
 
 
 def render_template(template_path: Path, context: dict[str, Any]) -> str:
